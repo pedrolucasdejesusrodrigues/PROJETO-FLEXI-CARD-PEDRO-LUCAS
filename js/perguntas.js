00b419f0c7c65dd10e012ea2b69125fc3590f1df
pedrolucasdejesusrodrigues <jesus.rodrigues.pedro@escola.pr.gov.br>
2025-08-06 14:39:28 -0300

    Create perguntas.js

---
+++ js/perguntas.js
@@ -0,0 +1,60 @@
+// Flashcards sobre Tatu Peba (Euphractus sexcinctus)
+criaCartao(
+    'Classificação',
+    'Qual é o nome científico do Tatu Peba?',
+    'O nome científico é Euphractus sexcinctus, da família Chlamyphoridae.'
+)
+
+criaCartao(
+    'Características',
+    'Quantas cintas móveis tem o Tatu Peba?',
+    'O Tatu Peba possui 6-8 cintas móveis, daí o nome "sexcinctus" (seis cintas).'
+)
+
+criaCartao(
+    'Habitat',
+    'Onde o Tatu Peba vive?',
+    'Vive em campos abertos, cerrados, caatingas e bordas de florestas na América do Sul.'
+)
+
+criaCartao(
+    'Alimentação',
+    'O que o Tatu Peba come?',
+    'É onívoro, alimentando-se de insetos, pequenos vertebrados, frutos, raízes e até carniça.'
+)
+
+criaCartao(
+    'Comportamento',
+    'O Tatu Peba é noturno ou diurno?',
+    'É principalmente diurno, ao contrário de muitas outras espécies de tatus.'
+)
+
+criaCartao(
+    'Reprodução',
+    'Quantos filhotes o Tatu Peba tem por gestação?',
+    'Geralmente 1-2 filhotes, com gestação de 60-65 dias.'
+)
+
+criaCartao(
+    'Defesa',
+    'Como o Tatu Peba se defende?',
+    'Pode se enrolar parcialmente e tem garras fortes para cavar e se defender.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'O Tatu Peba tem boa visão?',
+    'Não, tem visão pobre mas olfato e audição aguçados para encontrar comida.'
+)
+
+criaCartao(
+    'Conservação',
+    'O Tatu Peba está ameaçado?',
+    'Atualmente não é considerado ameaçado, mas sofre com caça e perda de habitat.'
+)
+
+criaCartao(
+    'Diferenças',
+    'Como diferenciar o Tatu Peba de outras espécies?',
+    'Pelos pelos amarelados entre as placas ósseas e pelas 6-8 cintas móveis no dorso.'
+)
